e5e545448afd76d98f4a36875e4faab607681b5e
N型 <u11b146h@gmail.com>
2022-06-05 16:24:03 +0900

    とりあえずコミット

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+//会員登録
+Route::get('the_white_board',[HomeController::class,'index']);
+Route::get('ver','API\VerController@index');
+
