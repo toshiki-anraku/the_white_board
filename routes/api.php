580efd64c4616399c0f54bd1eeedf5b4a5419113
N型 <u11b146h@gmail.com>
2022-07-29 01:18:13 +0900

    ProjectのCRUD API作成（序章）

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProjectController;
-use App\Http\Controllers\RegisterController;
-use App\Http\Controllers\LoginController;
-
 
 /*
 |--------------------------------------------------------------------------
@@ -18,26 +15,14 @@ use App\Http\Controllers\LoginController;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
-//会員登録
-Route::get('the_white_board',[ProjectController::class,'index']);
-Route::get('ver','API\VerController@index');
-// localhost/api/testにgetすると…
-Route::get('/test', function(){
-    return 'api is working!';
-});
-
-Route::post('/register', [RegisterController::class, 'register']);// ユーザー登録
-Route::post('/login', [LoginController::class, 'login']);// ログイン
-Route::post('/logout', [LoginController::class, 'logout']);// ログアウト
-Route::post('/projectList', [ProjectController::class, 'projectList']);//企画一覧
-Route::get('/user', [ProjectController::class, 'user']);//ユーザ情報
-Route::get('/home', [ProjectController::class, 'user']);//企画一覧
-Route::get('/project/{user_id}', [ProjectController::class, 'project']);//企画一覧
-Route::get('/like', [ProjectController::class, 'like']);//企画一覧
-Route::get('/favorite', [ProjectController::class, 'favorite']);//企画一覧
-Route::get('/secret_management', [ProjectController::class, 'secret_management']);//企画一覧
+// ログインしているユーザが閲覧可能な全企画データを取得
+Route::get('/projects', [ProjectController::class, 'index']);
+// 企画の作成
+Route::post('/projects', [ProjectController::class, 'store']);
+// 企画詳細の表示
+Route::get('/projects/{id}', [ProjectController::class, 'show']);
+// 企画の更新
+Route::put('/projects', [ProjectController::class, 'update']);
+// 企画の削除
+Route::delete('/projects', [ProjectController::class, 'destroy']);
 
