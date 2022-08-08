0505bc4c8da8f81fc4cea3075837b5faa1f3da28
toshiki-anraku <62826339+toshiki-anraku@users.noreply.github.com>
2022-08-09 08:04:10 +0900

    Bodyコンポーネント作成

--- resources/js/Pages/Mypage.vue
+++ resources/js/Pages/Mypage.vue
@@ -8,26 +8,21 @@
             </h2>
         </template>
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 bg-white border-b border-gray-200">
-                        マイページです
-                    </div>
-                </div>
-            </div>
-        </div>
+        <!-- ボディ -->
+        <Body>マイページです</Body>>
     </BreezeAuthenticatedLayout>
 </template>
 
 <script>
 import BreezeAuthenticatedLayout from "@/Layouts/Authenticated.vue";
 import { Head } from "@inertiajs/inertia-vue3";
+import Body from "@/OwnComponents/Organisms/Body.vue";
 
 export default {
     components: {
         BreezeAuthenticatedLayout,
         Head,
+        Body,
     },
 };
 </script>
--- resources/js/Pages/Top.vue
+++ resources/js/Pages/Top.vue
@@ -40,27 +40,22 @@
             </div>
         </template>
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 bg-white border-b border-gray-200">
-                        Topページです
-                    </div>
-                </div>
-            </div>
-        </div>
+        <!-- ボディ -->
+        <Body>トップページです</Body>>
     </TopLayout>
 </template>
 
 <script>
 import TopLayout from "@/Layouts/TopLayout.vue";
 import { Head, Link } from "@inertiajs/inertia-vue3";
+import Body from "@/OwnComponents/Organisms/Body.vue";
 
 export default {
     components: {
         TopLayout,
         Head,
         Link,
+        Body,
     },
     props: {
         canLogin: Boolean,
